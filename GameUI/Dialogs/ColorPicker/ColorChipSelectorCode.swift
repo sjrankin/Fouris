59d7ed39021412d98972c807f025e6680a57ca89
Stuart Rankin <sjrankin@gmail.com>
2019-08-31 17:40:01 +0900

    Create ColorChipSelectorCode.swift
    Initial implementation.

---
+++ GameUI/Dialogs/ColorPicker/ColorChipSelectorCode.swift
@@ -0,0 +1,32 @@
+//
+//  ColorChipSelectorCode.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 8/31/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorChipSelectorCode: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func HandleOKPressed(_ sender: Any)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func HandleCancelPressed(_ sender: Any)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func HandleSortPressed(_ sender: Any)
+    {
+    }
+}
