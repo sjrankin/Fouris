81f9a7f5cd978e268f52c6a9cef308f799de65f7
Stuart Rankin <sjrankin@gmail.com>
2019-08-31 17:38:25 +0900

    Create GSliderProtocol.swift
    Protocol for GSliders. Inherited from BumpCamera.

---
+++ GameUI/Dialogs/CustomControls/GSlider/GSliderProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  GSliderProtocol.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 8/30/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol GSliderProtocol: class
+{
+    func NewSliderValue(Name: String, NewValue: Double)
+}
