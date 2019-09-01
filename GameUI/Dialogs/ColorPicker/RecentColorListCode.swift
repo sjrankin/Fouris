6ff39b5628cecd894df2f5c7817d94947e4f5b71
Stuart Rankin <sjrankin@gmail.com>
2019-09-01 17:03:29 +0900

    Create RecentColorListCode.swift
    Added code to manage the recently used color list.

---
+++ GameUI/Dialogs/ColorPicker/RecentColorListCode.swift
@@ -0,0 +1,71 @@
+//
+//  RecentColorListCode.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/1/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecentColorListCode: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        RecentTable.layer.borderColor = UIColor.black.cgColor
+        PopulateColorList()
+        RecentTable.reloadData()
+    }
+    
+    private func PopulateColorList()
+    {
+        ColorList.removeAll()
+        for (Color, ColorName) in RecentlyUsedColors.ColorList
+        {
+            let ColorValue = ColorServer.MakeHexString(From: Color)
+            ColorList.append((Color, ColorName, ColorValue))
+        }
+    }
+    
+    private var ColorList = [(UIColor, String, String)]()
+    
+    @IBAction func HandleOKPressed(_ sender: Any)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func HandleCancelPressed(_ sender: Any)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func HandleClearList(_ sender: Any)
+    {
+        RecentlyUsedColors.Clear()
+        PopulateColorList()
+        RecentTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return RecentColorCell.CellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return ColorList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let Width = tableView.bounds.size.width
+        let Cell = RecentColorCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        let (Color, ColorName, ColorValue) = ColorList[indexPath.row]
+        Cell.LoadData(Color: Color, Name: ColorName, Value: ColorValue, Width: Width)
+        return Cell
+    }
+    
+    @IBOutlet weak var RecentTable: UITableView!
+}
