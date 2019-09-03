e705fc040b4b0ae3c21c5de1be60636cc15eee9f
Stuart Rankin <sjrankin@gmail.com>
2019-09-03 21:20:32 +0900

    Update Settings.swift
    Added GameBackgroundType setting and associated functions.

--- Common/Customization/Settings.swift
+++ Common/Customization/Settings.swift
@@ -66,8 +66,23 @@ class Settings
         _Settings.set(20, forKey: "MostRecentlyUsedColorCapacity")
         _Settings.set("", forKey: "MostRecentlyUsedColorList")
         _Settings.set(true, forKey: "ShowClosestColor")
+        _Settings.set(0, forKey: "GameBackgroundType")
         _Settings.set("83c630ee-81d4-11e9-bc42-526af7764f64", forKey: "CurrentTheme")
         _Settings.set("3f0d9fee-0b77-465b-a0ac-f1663da23cc9", forKey: "Current3DTheme")
+    }
+    
+    /// Returns the game background type.
+    /// - Returns: Value indicating the game background type.
+    public static func GetGameBackgroundType() -> Int
+    {
+        return _Settings.integer(forKey: "GameBackgroundType")
+    }
+    
+    /// Set the game background type value. No checking is done here.
+    /// - Parameter NewValue: New game background type index.
+    public static func SetGameBackgroundType(NewValue: Int)
+    {
+        _Settings.set(NewValue, forKey: "GameBackgroundType")
     }
     
     /// Returns the show closest color flag for the color name picker.
