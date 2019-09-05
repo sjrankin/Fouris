6f519786d0e108454313a97039ce1cdaf4cef1fb
Stuart Rankin <sjrankin@gmail.com>
2019-09-05 21:38:27 +0900

    Create ThemeEditingProtocol.swift
    Added protocol for the editing and creation of themes.

---
+++ GameUI/Dialogs/ThemeEditingProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeEditingProtocol.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/5/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Protocol for editing themes.
+protocol ThemeEditingProtocol: class
+{
+    /// Used to send to an editor the ID of the theme to edit.
+    func EditTheme(ID: UUID)
+    
+    /// Used to send to a caller the results of editing a theme.
+    func EditResults(_ Edited: Bool, ThemeID: UUID)
+}
