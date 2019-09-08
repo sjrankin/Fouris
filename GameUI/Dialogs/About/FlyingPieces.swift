ef5264ce5db95657e9736040cf897e10b254c3c0
Stuart Rankin <sjrankin@gmail.com>
2019-09-08 22:54:48 +0900

    Create FlyingPieces.swift
    Stub of FlyingPieces code.

---
+++ GameUI/Dialogs/About/FlyingPieces.swift
@@ -0,0 +1,31 @@
+//
+//  FlyingPieces.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/8/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+
+class FlyingPieces: SCNView
+{
+    /// Required by framework.
+    /// - Parameter coder: See Apple documentation.
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+    }
+    
+    public func Play(PieceCount: Int)
+    {
+        
+    }
+    
+    public func Stop()
+    {
+        
+    }
+}
