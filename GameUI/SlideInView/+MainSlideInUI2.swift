db3c9a6a79ffb6a08fb48d32c17269cd061eff25
Stuart Rankin <sjrankin@gmail.com>
2019-09-10 22:27:40 +0900

    Update +MainSlideInUI2.swift
    Most code removed. Will probably be subsumed into the main controller code later on.

--- GameUI/SlideInView/+MainSlideInUI2.swift
+++ GameUI/SlideInView/+MainSlideInUI2.swift
@@ -10,60 +10,11 @@ import Foundation
 import UIKit
 
 /// Extension to **CommonViewController** to handle events and user interactions related to the slide in view.
-extension MainViewController2: UITableViewDelegate, UITableViewDataSource
+extension MainViewController2
 {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    public func InitializeSlideIn()
     {
-        return CommandList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        return CommandList[indexPath.row] as UITableViewCell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
-    {
-        return SlideInItem.CellHeight
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        if let Cell = tableView.cellForRow(at: indexPath) as? SlideInItem
-        {
-            HandleSlideInCommand(Cell.CommandID)
-        }
-    }
-    
-    /// Initialize the visuals and content of the table used to display options.
-    /// - Parameter Table: The UITableView to initialize.
-    public func InitializeOptionTable(_ Table: UITableView)
-    {
-        Table.layer.borderColor = UIColor.black.cgColor
-        Table.layer.borderWidth = 1.0
-        Table.layer.cornerRadius = 5.0
-        AddCommands()
-        Table.reloadData()
-    }
-    
-    /// Add commands to the slide in menu/table.
-    private func AddCommands()
-    {
-        let AboutItem = SlideInItem(style: UITableViewCell.CellStyle.default, reuseIdentifier: "AboutItemIdentifier")
-        AboutItem.SetCommand(CommandName: "About", ID: CommandIDs[.AboutCommand]!)
-        CommandList.append(AboutItem)
-        
-        let GameStyleItem = SlideInItem(style: UITableViewCell.CellStyle.default, reuseIdentifier: "SelectGame")
-        GameStyleItem.SetCommand(CommandName: "Select Game", ID: CommandIDs[.SelectGameCommand]!)
-        CommandList.append(GameStyleItem)
-        
-        let ThemeItem = SlideInItem(style: UITableViewCell.CellStyle.default, reuseIdentifier: "ThemeItem")
-        ThemeItem.SetCommand(CommandName: "Themes", ID: CommandIDs[.ThemeCommand]!)
-        CommandList.append(ThemeItem)
-        
-        let SettingsItem = SlideInItem(style: UITableViewCell.CellStyle.default, reuseIdentifier: "SettingsItem")
-        SettingsItem.SetCommand(CommandName: "Settings", ID: CommandIDs[.SettingsCommand]!)
-        CommandList.append(SettingsItem)
+        SlideInSubView.layer.borderColor = ColorServer.CGColorFrom(ColorNames.ReallyDarkGray)
     }
 }
 
