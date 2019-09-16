61b0b3699d253eba1c26fa8838a268eca3712aab
Stuart Rankin <sjrankin@gmail.com>
2019-09-16 21:58:41 +0900

    Create IntCell.swift
    Table cell for editing integer field data.

---
+++ GameUI/Dialogs/RawTheme/FieldCells/IntCell.swift
@@ -0,0 +1,54 @@
+//
+//  IntCell.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/16/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IntCell: FieldCell
+{
+    override func DrawUI()
+    {
+        if !WasInitialized
+        {
+            return
+        }
+        FieldLabel = UILabel(frame: CGRect(x: 5, y: 3, width: ParentWidth / 2, height: 69))
+        contentView.addSubview(FieldLabel!)
+        FieldLabel?.text = FieldTitle
+        StyleTitle(FieldLabel!)
+        let TextX = ParentWidth - 200
+        TextBox = UITextField(frame: CGRect(x: TextX, y: 15, width: 170, height: 40))
+        StyleTextBox(TextBox)
+        contentView.addSubview(TextBox)
+        TextBox.text = "\(Current as! Int)"
+        TextBox.addTarget(self, action: #selector(HandleTextChange), for: UIControl.Event.valueChanged)
+    }
+    
+    var TextBox: UITextField!
+    
+    @objc func HandleTextChange(sender: UITextField)
+    {
+        if let Raw = TextBox.text
+        {
+            if let RawValue = Int(Raw)
+            {
+                Current = RawValue
+                if let Handler = ChangeHandler
+                {
+                    Handler(Current as Any)
+                    return
+                }
+                FieldDelegate?.EditedField(ID, NewValue: Current as Any, DefaultValue: Default as Any, FieldType: FieldType)
+            }
+            else
+            {
+                TextBox.text = ""
+            }
+        }
+    }
+}
