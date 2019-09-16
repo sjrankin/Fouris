25eac50551d820adc8f7663b3f184db125722fa0
Stuart Rankin <sjrankin@gmail.com>
2019-09-16 14:44:37 +0900

    Update FlyingProtocol.swift
    Added comments.

--- GameUI/Dialogs/About/FlyingPieces/FlyingProtocol.swift
+++ GameUI/Dialogs/About/FlyingPieces/FlyingProtocol.swift
@@ -10,7 +10,11 @@ import Foundation
 import UIKit
 import SceneKit
 
+/// Protocol for communicating from a flying piece to its parent scene.
 protocol FlyingProtocol: class
 {
+    /// A flying piece completed its motion (and any terminal effects).
+    /// - Parameter Node: The node that completed its motion.
+    /// - Parameter Replace: Boolean that determines if the node should be replaced by a new node.
     func MotionCompleted(Node: SCNNode, Replace: Bool)
 }
