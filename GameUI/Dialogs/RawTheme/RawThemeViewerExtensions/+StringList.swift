de721d98bfde37005ce22e6757648987bafa6efa
Stuart Rankin <sjrankin@gmail.com>
2019-09-18 22:17:28 +0900

    Create +StringList.swift
    Handles populating the string list view.

---
+++ GameUI/Dialogs/RawTheme/RawThemeViewerExtensions/+StringList.swift
@@ -0,0 +1,39 @@
+//
+//  +StringList.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/18/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension RawThemeViewerCode2
+{
+    func PopulateStringListView(WithField: GroupField2)
+    {
+        StringListPicker.layer.borderColor = UIColor.black.cgColor
+        StringListPicker.layer.borderWidth = 0.5
+        StringListPicker.layer.cornerRadius = 5.0
+        StringListDescription.clipsToBounds = true
+        StringListDescription.layer.cornerRadius = 4.0
+        
+        StringListTitle.text = WithField.Title
+        StringListDescription.text = WithField.Description
+        StringListData = WithField.StringList
+        LastSelectedPickerViewItem = nil
+        StringListPicker.reloadAllComponents()
+        if let CurrentValue = WithField.Starting as? String
+        {
+            if let Index = StringListData.firstIndex(of: CurrentValue)
+            {
+                StringListPicker.selectRow(Index, inComponent: 0, animated: true)
+            }
+        }
+        
+        CurrentField = WithField
+        ShowViewType(WithField.FieldType)
+        StringListViewDirty.alpha = 0.0
+    }
+}
