ebfb3d7678da6c2fe439ec53d1233bf46906d417
Stuart Rankin <sjrankin@gmail.com>
2019-09-19 17:37:23 +0900

    Create +Color.swift
    Color field view.

---
+++ GameUI/Dialogs/RawTheme/RawThemeViewerExtensions/+Color.swift
@@ -0,0 +1,38 @@
+//
+//  +Color.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/19/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension RawThemeViewerCode2
+{
+    func PopulateColorView(WithField: GroupField2)
+    {
+        ColorDescription.layer.cornerRadius = 4.0
+        ColorDescription.clipsToBounds = true
+        
+        let StartColor = WithField.Starting as? UIColor
+        let ColorNames = PredefinedColors.NamesFrom(FindColor: StartColor!)
+        let ColorName: String? = ColorNames.count > 0 ? ColorNames[0] : nil
+        if let FinalColorName = ColorName
+        {
+            ColorControlTitle.text = FinalColorName
+        }
+        else
+        {
+            ColorControlTitle.text = ""
+        }
+        ColorTitle.text = WithField.Title
+        ColorDescription.text = WithField.Description
+        ColorSwatch.TopColor = StartColor!
+        
+        CurrentField = WithField
+        ShowViewType(WithField.FieldType)
+        ColorViewDirty.alpha = 0.0
+    }
+}
