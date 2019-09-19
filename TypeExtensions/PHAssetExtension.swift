1771172c31b09e4d50cfa78146d2e009843238a3
Stuart Rankin <sjrankin@gmail.com>
2019-09-19 17:31:26 +0900

    Create PHAssetExtension.swift
    Extension for PHAsset to return the original file name easily.

---
+++ TypeExtensions/PHAssetExtension.swift
@@ -0,0 +1,21 @@
+//
+//  PHAssetExtension.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/19/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+/// Extensions for PHAssets.
+extension PHAsset
+{
+    /// Returns the original file name from the instance asset.
+    var OriginalFileName: String?
+    {
+        return PHAssetResource.assetResources(for: self).first?.originalFilename
+    }
+}
