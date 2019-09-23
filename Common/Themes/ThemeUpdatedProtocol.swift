7305f5d76be319dbe1322890d46afd13f7d72756
Stuart Rankin <sjrankin@gmail.com>
2019-09-23 21:48:54 +0900

    Update ThemeUpdatedProtocol.swift
    Changed FieldName parameter to Field of a different type.

--- Common/Themes/ThemeUpdatedProtocol.swift
+++ Common/Themes/ThemeUpdatedProtocol.swift
@@ -13,9 +13,9 @@ import UIKit
 /// need to change immediately.
 protocol ThemeUpdatedProtocol
 {
-    /// Called when a theme is updated.
-    /// - Note: Called at each change, even if the field is changed back to the original value.
+    /// Called when a theme field changes when someone sets its property.
+    /// - Note: Called at each change even if the field is changed back to the original value.
     /// - Parameter ThemeName: Name of the changed theme.
-    /// - Parameter FieldName: The name of the property that was updated.
-    func ThemeUpdated(ThemeName: String, FieldName: String)
+    /// - Parameter Field: The field that changed.
+    func ThemeUpdated(ThemeName: String, Field: ThemeFields)
 }
