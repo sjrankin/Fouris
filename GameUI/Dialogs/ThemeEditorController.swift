4e137a8aae1068110410f2360dc4bc791ac2e427
Stuart Rankin <sjrankin@gmail.com>
2019-09-23 17:53:39 +0900

    Update ThemeEditorController.swift
    Updated to be the main entry point into settings.

--- GameUI/Dialogs/ThemeEditorController.swift
+++ GameUI/Dialogs/ThemeEditorController.swift
@@ -16,12 +16,17 @@ class ThemeEditorController: UIViewController, ThemeEditingProtocol
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        SettingsBox.layer.borderColor = UIColor.black.cgColor
+        SettingsBox.layer.backgroundColor = ColorServer.CGColorFrom(ColorNames.WhiteSmoke)
         BackgroundView.layer.borderColor = UIColor.black.cgColor
         BackgroundView.layer.backgroundColor = ColorServer.CGColorFrom(ColorNames.WhiteSmoke)
         BucketSettingsView.layer.borderColor = UIColor.black.cgColor
         BucketSettingsView.layer.backgroundColor = ColorServer.CGColorFrom(ColorNames.WhiteSmoke)
         PiecesView.layer.borderColor = UIColor.black.cgColor
         PiecesView.layer.backgroundColor = ColorServer.CGColorFrom(ColorNames.WhiteSmoke)
+        
+        ShowBucketGridSwitch.isOn = UserTheme!.ShowBucketGrid
+        ShowBucketBoundsSwitch.isOn = UserTheme!.ShowBucketGridOutline
     }
     
     func EditTheme(Theme: ThemeDescriptor, DefaultTheme: ThemeDescriptor)
@@ -37,12 +42,15 @@ class ThemeEditorController: UIViewController, ThemeEditingProtocol
     }
     
     var UserTheme: ThemeDescriptor? = nil
+
     var DefaultTheme: ThemeDescriptor? = nil
     
     func EditResults(_ Edited: Bool, ThemeID: UUID, PieceID: UUID?)
     {
         //do something here
     }
+    
+    // MARK: Dialog instantiations.
     
     @IBSegueAction func InstantiateGameBackground(_ coder: NSCoder) -> GameBackgroundDialog?
     {
@@ -67,18 +75,33 @@ class ThemeEditorController: UIViewController, ThemeEditingProtocol
         return Editor
     }
     
-    @IBAction func HandleSaveButtonPressed(_ sender: Any)
+    @IBSegueAction func InstantiateSettingsEditor(_ coder: NSCoder) -> SettingsControllerCode?
+    {
+        let Editor = SettingsControllerCode(coder: coder)
+        return Editor
+    }
+    
+    // MARK: Bucket grid controls.
+    
+    @IBAction func HandleShowBucketGridChanged(_ sender: Any)
+    {
+        UserTheme!.ShowBucketGrid = ShowBucketGridSwitch.isOn
+    }
+    
+    @IBAction func HandleShowBucketGridOutlineChanged(_ sender: Any)
+    {
+        UserTheme!.ShowBucketGridOutline = ShowBucketBoundsSwitch.isOn
+    }
+    
+    // MARK: UI control handling.
+    
+    @IBAction func HandleCloseButtonPressed(_ sender: Any)
     {
         ThemeDelegate?.EditResults(true, ThemeID: UserTheme!.ID, PieceID: nil)
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func HandleCancelButtonPressed(_ sender: Any)
-    {
-        ThemeDelegate?.EditResults(false, ThemeID: UserTheme!.ID, PieceID: nil)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+    @IBOutlet weak var SettingsBox: UIView!
     @IBOutlet weak var RawThemeViewButton: UIButton!
     @IBOutlet weak var PiecesView: UIView!
     @IBOutlet weak var BackgroundView: UIView!
