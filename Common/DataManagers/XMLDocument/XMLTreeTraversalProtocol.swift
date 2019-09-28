3fb82c598a01113f7815729e69e8695136af14a2
Stuart Rankin <sjrankin@gmail.com>
2019-09-28 21:48:42 +0900

    Create XMLTreeTraversalProtocol.swift
    Protocol for the traversal of XML trees.

---
+++ Common/DataManagers/XMLDocument/XMLTreeTraversalProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  XMLTreeTraversalProtocol.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol XMLTreeTraversalProtocol
+{
+    func AtNode(Node: XMLNode)
+}
