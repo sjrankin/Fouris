9c564bf88b49c632d7ceb1153c211b7996307ca7
Stuart Rankin <sjrankin@gmail.com>
2019-09-28 21:48:20 +0900

    Create XMLDeserializeProtocol.swift
    Protocol for the deserialization of XML trees into whatever class the caller wants.

---
+++ Common/DataManagers/XMLDocument/XMLDeserializeProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  XMLDeserializeProtocol.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol XMLDeserializeProtocol
+{
+    func DeserializedNode(_ Node: XMLNode)
+}
