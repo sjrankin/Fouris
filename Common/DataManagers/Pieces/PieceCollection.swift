e3587675e8cae176533db80a035ef03dd0361f11
Stuart Rankin <sjrankin@gmail.com>
2019-09-28 21:44:38 +0900

    Create PieceCollection.swift
    Class that contains a collection of piece classes. Also responsible for the deserialization of stored piece information.

---
+++ Common/DataManagers/Pieces/PieceCollection.swift
@@ -0,0 +1,144 @@
+//
+//  PieceCollection.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Holds a collection of piece definitions.
+class PieceCollection: XMLDeserializeProtocol
+{
+    init()
+    {
+        _Classes = [PieceClasses: [PieceDefinition2]]()
+    }
+    
+    private var _Classes: [PieceClasses: [PieceDefinition2]] = [PieceClasses: [PieceDefinition2]]()
+    public var Classes: [PieceClasses: [PieceDefinition2]]
+    {
+        get
+        {
+            return _Classes
+        }
+        set
+        {
+            _Classes = newValue
+        }
+    }
+    
+    public func GetPieceClass(_ PieceClass: PieceClasses) -> [PieceDefinition2]?
+    {
+        return Classes[PieceClass]
+    }
+    
+    // MARK: Serialization and deserialization.
+    
+    /// Deserialize from the passed node.
+    func DeserializedNode(_ Node: XMLNode)
+    {
+        switch Node.Name
+        {
+            case "Pieces":
+                for PieceNode in Node.Children
+                {
+                    if PieceNode.Name == "PieceClass"
+                    {
+                        let ClassName = XMLNode.GetAttributeNamed("Type", InNode: PieceNode)!
+                        print(" PieceClass: \(ClassName)")
+                        let PieceClass = PieceClasses(rawValue: ClassName)!
+                        if _Classes[PieceClass] == nil
+                        {
+                            _Classes[PieceClass] = [PieceDefinition2]()
+                        }
+                        for Child in PieceNode.Children
+                        {
+                            if Child.Name == "Piece"
+                            {
+                                let PieceName = XMLNode.GetAttributeNamed("Name", InNode: Child)!
+                                print("  Piece: \(PieceName)")
+                                let RawPieceID = XMLNode.GetAttributeNamed("ID", InNode: Child)!
+                                let PieceID = UUID(uuidString: RawPieceID)!
+                                let NewPiece = PieceDefinition2()
+                                NewPiece.ID = PieceID
+                                NewPiece.PieceClass = PieceClass
+                                NewPiece.Name = PieceName
+                                _Classes[PieceClass]?.append(NewPiece)
+                                for PieceChild in Child.Children
+                                {
+                                    switch PieceChild.Name
+                                    {
+                                        case "UserPiece":
+                                            let RawIsUser = XMLNode.GetAttributeNamed("UserDefined", InNode: PieceChild)!
+                                            let IsUser = Bool(RawIsUser)!
+                                            NewPiece.IsUserPiece = IsUser
+                                        
+                                        case "Geometry":
+                                            let RawThin = XMLNode.GetAttributeNamed("Thin", InNode: PieceChild)!
+                                            let IsThin = Int(RawThin)!
+                                            NewPiece.ThinOrientation = IsThin
+                                            let RawWide = XMLNode.GetAttributeNamed("Wide", InNode: PieceChild)!
+                                            let IsWide = Int(RawWide)!
+                                            NewPiece.WideOrientation = IsWide
+                                            let RawSymmetry = XMLNode.GetAttributeNamed("Symmetric", InNode: PieceChild)!
+                                            let RotationallySymmetric = Bool(RawSymmetry)!
+                                            NewPiece.RotationallySymmetric = RotationallySymmetric
+                                        
+                                        case "LogicalLocations":
+                                            for Location in PieceChild.Children
+                                            {
+                                                let NewLocation = PieceBlockLocation()
+                                                let RawIndex = XMLNode.GetAttributeNamed("Index", InNode: Location)!
+                                                let Index = Int(RawIndex)!
+                                                NewLocation.Index = Index
+                                                let RawOrigin = XMLNode.GetAttributeNamed("IsOrigin", InNode: Location)!
+                                                let IsOrigin = Bool(RawOrigin)!
+                                                NewLocation.IsOrigin = IsOrigin
+                                                let RawXY = XMLNode.GetAttributeNamed("XY", InNode: Location)!
+                                                let Parts = RawXY.split(separator: ",", omittingEmptySubsequences: true)
+                                                if Parts.count != 2
+                                                {
+                                                    fatalError("Found invalid XY coordinate: \(RawXY)")
+                                                }
+                                                
+                                                var X: Int = 0
+                                                var Y: Int = 0
+                                                if let PX = Int(String(Parts[0]))
+                                                {
+                                                    X = PX
+                                                }
+                                                else
+                                                {
+                                                    fatalError("Invalid X coordinate in \(RawXY)")
+                                                }
+                                                if let PY = Int(String(Parts[1]))
+                                                {
+                                                    Y = PY
+                                                }
+                                                else
+                                                {
+                                                    fatalError("Invalid Y coordinate in \(RawXY)")
+                                                }
+                                                let NewPoint = Point3D<Int>(X, Y)
+                                                print("   Location (\(X),\(Y))")
+                                                NewLocation.Coordinates = NewPoint
+                                                NewPiece.Locations.append(NewLocation)
+                                        }
+                                        
+                                        default:
+                                            break
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }
+            
+            default:
+                return
+        }
+    }
+}
