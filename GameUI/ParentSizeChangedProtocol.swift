83ea2bd46eb48bb4b290d21876e2cb30272ed108
Stuart Rankin <sjrankin@gmail.com>
2019-09-29 22:55:20 +0900

    Create ParentSizeChangedProtocol.swift
    Test implementation of a way to tell child views their parent has resized. May not use and if not, will delete.

---
+++ GameUI/ParentSizeChangedProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ParentSizeChangedProtocol.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 9/29/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ParentSizeChangedProtocol
+{
+    func NewParentSize(Bounds: CGRect, Frame: CGRect)
+}
