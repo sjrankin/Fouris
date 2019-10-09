3523e9f9e3299385f42da89ab64dd220366e29f1
Stuart Rankin <sjrankin@gmail.com>
2019-10-09 12:35:56 +0900

    Create AttributionData.swift
    Will eventially hold attributions and credits text.

---
+++ GameUI/Dialogs/About/AttributionData.swift
@@ -0,0 +1,18 @@
+//
+//  AttributionData.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 10/8/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+class AttributeData
+{
+    public static var List: [(String, String)] =
+    [
+        ("Test","Data"),
+        ("Fred","Flintstone")
+    ]
+}
