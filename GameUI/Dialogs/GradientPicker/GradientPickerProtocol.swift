82aba5ba35744997431b4169b7b8f12e599c3812
Stuart Rankin <sjrankin@gmail.com>
2019-10-09 21:26:55 +0900

    Update GradientPickerProtocol.swift
    Added comments.

--- GameUI/Dialogs/GradientPicker/GradientPickerProtocol.swift
+++ GameUI/Dialogs/GradientPicker/GradientPickerProtocol.swift
@@ -10,11 +10,21 @@
 import Foundation
 import UIKit
 
+/// Protocol for handling gradients with respect to editing.
 protocol GradientPickerProtocol: class
 {
+    /// Called when a gradient has been edited.
+    /// - Parameter Edited: The edited gradient. If nil, the edit operation was canceled.
+    /// - Parameter Tag: Caller-supplied tag value returned to the caller. Can be used to
+    ///                  keep track of multiple gradient editing requests.
     func EditedGradient(_ Edited: String?, Tag: Any?)
     
+    /// Called to set a gradient to edit.
+    /// - Parameter Edited: The gradient to edit. If nil, default gradient will be used.
+    /// - Parameter Tag: Tag value that is echoed back on a corresponding `EditedGradient` call.
     func GradientToEdit(_ Edited: String?, Tag: Any?)
-    
+
+    /// Set a color stop.
+    /// - Parameter StopColorIndex: The color index for the stop.
     func SetStop(StopColorIndex: Int)
 }
