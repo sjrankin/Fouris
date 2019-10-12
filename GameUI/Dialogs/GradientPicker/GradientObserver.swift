b9097e38e5e5ad993ced6c1a9a68be6481a4998d
Stuart Rankin <sjrankin@gmail.com>
2019-10-12 17:26:42 +0900

    Create GradientObserver.swift
    Protocol for changes to gradients.

---
+++ GameUI/Dialogs/GradientPicker/GradientObserver.swift
@@ -0,0 +1,14 @@
+//
+//  GradientObserver.swift
+//  Fouris
+//
+//  Created by Stuart Rankin on 10/12/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol GradientObserver: class
+{
+    func GradientChanged(NewGradient: String)
+}
