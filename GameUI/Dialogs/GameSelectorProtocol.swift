ddfca52000cbddf06ee43b78c942d73f3bf19610
Stuart Rankin <sjrankin@gmail.com>
2019-10-16 16:35:59 +0900

    Update GameSelectorProtocol.swift
    Removed NewBaseType and GameSubType from parameter list. Replaced them with NewGameShape.

--- GameUI/Dialogs/GameSelectorProtocol.swift
+++ GameUI/Dialogs/GameSelectorProtocol.swift
@@ -11,11 +11,8 @@ import UIKit
 
 protocol GameSelectorProtocol: class
 {
-    /// Called when the game selector dialog closes.
-    /// - Parameter DidChange: If true, the game type or sub type or both changed.
-    /// - Parameter NewBaseType: The new base type (or old one if only the `GameSubType` changed). If `DidChange` is false,
-    ///                          this value will be nil.
-    /// - Parameter GameSubType: The new sub type game (or old one if only the `NewBaseType` changed). If `DidChange` is false,
-    ///                          this value will be nil.
-    func GameTypeChanged(DidChange: Bool, NewBaseType: BaseGameTypes?, GameSubType: BaseGameSubTypes?)
+    /// Called when the game selector dialog is closed.
+    /// - Parameter DidChange: If true, the game shape changed. If false, the user closed the dialog with a the `Cancel` button.
+    /// - Parameter NewGameShape: The new shape of the game.
+    func GameTypeChanged(DidChange: Bool, NewGameShape: BucketShapes?)
 }
