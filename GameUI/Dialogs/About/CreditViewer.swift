1c76d3c64823bb9c5aba8418fc5d05bf796aef64
Stuart Rankin <sjrankin@gmail.com>
2019-10-20 10:49:57 +0900

    Update CreditViewer.swift
    Refactored such that headers live in a table that the user can select which wil then show the associated legal/licensing/credits.

--- GameUI/Dialogs/About/CreditViewer.swift
+++ GameUI/Dialogs/About/CreditViewer.swift
@@ -9,20 +9,44 @@
 import Foundation
 import UIKit
 
-class CreditViewer: UIViewController
+class CreditViewer: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        TitleTable.layer.borderColor = UIColor.black.cgColor
         AttributionBox.layer.borderColor = UIColor.black.cgColor
         AttributionBox.layer.backgroundColor = UIColor.clear.cgColor
         AttributionsViewer.text = ""
-        var Final = ""
-        for (Title, Text) in AttributeData.List
+        for (Title, Text) in AttributeData.Table
         {
-            Final.append(Title + " " + Text + "\n")
+            TableTitleTable.append((Title,Text))
         }
-        AttributionsViewer.text = Final
+        AttributionsViewer.text = ""
+    }
+    
+    var TableTitleTable = [(Title: String, Text: String)]()
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return TitleTableCell.CellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return TableTitleTable.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let Cell = TitleTableCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "TitleTableCell")
+        Cell.Initialize(WithTitle: TableTitleTable[indexPath.row].Title, TableWidth: TitleTable.bounds.width)
+        return Cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        AttributionsViewer.text = TableTitleTable[indexPath.row].Text
     }
     
     @IBAction func HandleCloseButton(_ sender: Any)
@@ -30,6 +54,7 @@ class CreditViewer: UIViewController
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var TitleTable: UITableView!
     @IBOutlet weak var AttributionBox: UIView!
     @IBOutlet weak var AttributionsViewer: UITextView!
 }
