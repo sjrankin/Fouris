11e72487bc3d19a633b396d7ce58c36391a42665
Stuart Rankin <sjrankin@gmail.com>
2019-10-24 14:21:24 +0900

    Update AppDelegate.swift
    Added home screen menu handling.

--- GameUI/AppDelegate.swift
+++ GameUI/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Foundation
 
 @UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate
 {
+    /// The main UIWindow.
     var window: UIWindow?
     
+    var StartupShortcut: UIApplicationShortcutItem? = nil
+    
+    /// Application launch tasks. Capture launch tasks from the home screen menu.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        // Override point for customization after application launch.
         #if targetEnvironment(simulator)
         UserDefaults.standard.set(true, forKey: "RunningOnSimulator")
         #else
@@ -26,13 +30,25 @@ import UIKit
             print("Running on simulator.")
         }
         UIApplication.shared.isIdleTimerDisabled = true
+        if let ShortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem
+        {
+            StartupShortcut = ShortcutItem
+        }
         return true
     }
     
+    /// Alternatively, a shortcut item may be passed in through this delegate method if the app was
+    /// still in memory when the Home screen quick action was used. Again, store it for processing.
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void)
+    {
+        StartupShortcut = shortcutItem
+    }
+   
+    /// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions
+    /// (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    /// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     func applicationWillResignActive(_ application: UIApplication)
     {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         if let GameView = self.window?.rootViewController as? MainViewController
         {
             if !GameView.MakingVideo
@@ -46,35 +62,46 @@ import UIKit
         UIApplication.shared.isIdleTimerDisabled = false
     }
     
+    /// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your
+    /// application to its current state in case it is terminated later. If your application supports background execution, this method is called
+    /// instead of applicationWillTerminate: when the user quits.
     func applicationDidEnterBackground(_ application: UIApplication)
     {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         var NotUsed: String? = nil
         ActivityLog.AddEntry(Title: "System", Source: "AppDelegate", KVPs: [("Message","Fouris entered background.")], LogFileName: &NotUsed)
         ActivityLog.SaveLog()
         UIApplication.shared.isIdleTimerDisabled = false
     }
     
+    /// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     func applicationWillEnterForeground(_ application: UIApplication)
     {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         UIApplication.shared.isIdleTimerDisabled = true
         var NotUsed: String? = nil
         ActivityLog.AddEntry(Title: "System", Source: "AppDelegate", KVPs: [("Message","Fouris entered background.")], LogFileName: &NotUsed)
     }
     
+    /// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background,
+    /// optionally refresh the user interface. Process any short cut commands.
     func applicationDidBecomeActive(_ application: UIApplication)
     {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let ShortcutItem = StartupShortcut
+        {
+            print("Encountered short cut item \(ShortcutItem.type)")
+            let Message = "\(ShortcutItem.type) triggered."
+            let Alert = UIAlertController(title: "Quick Action", message: Message, preferredStyle: .alert)
+            Alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            window?.rootViewController?.present(Alert, animated: true, completion: nil)
+            StartupShortcut = nil
+        }
         UIApplication.shared.isIdleTimerDisabled = true
         var NotUsed: String? = nil
         ActivityLog.AddEntry(Title: "System", Source: "AppDelegate", KVPs: [("Message","Fouris became active.")], LogFileName: &NotUsed)
     }
     
+    /// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     func applicationWillTerminate(_ application: UIApplication)
     {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         var NotUsed: String? = nil
         ActivityLog.AddEntry(Title: "System", Source: "AppDelegate", KVPs: [("Message","Fouris terminated.")], LogFileName: &NotUsed)
         ActivityLog.SaveLog()
