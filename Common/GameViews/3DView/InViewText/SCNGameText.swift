9db2b142cf94ce69c248816a853c39ab25a82898
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:35:36 +0900

    Update SCNGameText.swift
    Updated comments.

--- Common/GameViews/3DView/InViewText/SCNGameText.swift
+++ Common/GameViews/3DView/InViewText/SCNGameText.swift
@@ -11,6 +11,7 @@ import UIKit
 import SceneKit
 
 /// Extends (slightly) the `SCNNode` class to wrap text to make displaying message in the game view more efficient.
+/// - Note: Not currently used as NSAttributedStrings do not display well in SCNText objects.
 class SCNGameText: SCNNode
 {
     /// Initializer.
@@ -28,6 +29,12 @@ class SCNGameText: SCNNode
         super.init(coder: coder)
     }
     
+    /// Changes the text of the node.
+    /// - Parameter To: The new text for the node.
+    /// - Parameter With: Depth of the extrusion.
+    /// - Parameter TextColor: Diffuse color of the node.
+    /// - Parameter Specular: Specular color of the node.
+    /// - Parameter Font: Font to use for the text.
     public func ChangeText(To NewText: String, With Depth: CGFloat, TextColor: UIColor,
                            Specular: UIColor, Font: UIFont)
     {
@@ -39,6 +46,10 @@ class SCNGameText: SCNNode
         self.geometry = TextGeo
     }
     
+    /// Changes the text of the node.
+    /// - Parameter To: Attributed string to use for the text.
+    /// - Parameter With: Extrusion depth of the text.
+    /// - Parameter Specular: Specular color of the node.
     public func ChangeTextEx(To NewText: NSAttributedString, With Depth: CGFloat, Specular: UIColor)
     {
         let TextGeo = SCNText(string: NewText, extrusionDepth: Depth)
@@ -47,6 +58,15 @@ class SCNGameText: SCNNode
         self.geometry = TextGeo
     }
     
+    /// Factory method to create a populated `SCNGameText` node. Uses **String** as the source text.
+    /// - Parameter SourceText: The text to display.
+    /// - Parameter Font: The font to use for the text.
+    /// - Parameter TextColor: The color of the text (acts as the source for the diffuse surface).
+    /// - Parameter Specular: The specular color of the node.
+    /// - Parameter Scale: The scale factor for the text.
+    /// - Parameter Depth: The extrusion depth.
+    /// - Parameter Location: The position of the text node in its parent scene.
+    /// - Parameter LightMask: The light mask to use.
     public static func MakeGameText(_ SourceText: String, Font: UIFont, TextColor: UIColor = UIColor.white,
                                     Specular: UIColor = UIColor.white, Scale: CGFloat, Depth: CGFloat,
                                     Location: SCNVector3, LightMask: Int) -> SCNGameText
@@ -63,6 +83,13 @@ class SCNGameText: SCNNode
         return Node
     }
     
+    /// Factory method to create a populated `SCNGameText` node. Uses **NSAttributedString** as the source text.
+    /// - Parameter SourceText: The text to display.
+    /// - Parameter Specular: The specular color of the node.
+    /// - Parameter Scale: The scale factor for the text.
+    /// - Parameter Depth: The extrusion depth.
+    /// - Parameter Location: The position of the text node in its parent scene.
+    /// - Parameter LightMask: The light mask to use.
     public static func MakeGameText(_ SourceText: NSAttributedString, Specular: UIColor = UIColor.white,
                                     Scale: CGFloat, Depth: CGFloat, Location: SCNVector3,
                                     LightMask: Int) -> SCNGameText
