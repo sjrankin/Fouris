eefe544d560fa49e1ff273c3391971177e3f0383
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 20:25:09 +0900

    Update ControlUIProtocol.swift
    Updated comments.

--- Common/Protocols/ControlUIProtocol.swift
+++ Common/Protocols/ControlUIProtocol.swift
@@ -1,6 +1,6 @@
 //
 //  ControlUIProtocol.swift
-//  WackyDesktopTetris
+//  Fouris
 //
 //  Created by Stuart Rankin on 4/29/19.
 //  Copyright © 2019 Stuart Rankin. All rights reserved.
@@ -13,29 +13,24 @@ import UIKit
 protocol ControlUIProtocol: class
 {
     /// Handle game over events.
-    ///
     /// - Parameters:
     ///   - FinalScore: Final game score.
     ///   - HighScore: Final high score.
     func GameOver(FinalScore: Int, HighScore: Int)
     
     /// Sets the next piece.
-    ///
     /// - Parameter NextPiece: The next piece to play.
     func SetNextPiece(NextPiece: Piece)
     
     /// New high score value.
-    ///
     /// - Parameter NewHighScore: The new high score.
     func SetHighScore(NewHighScore: Int)
     
     /// New game score value.
-    ///
     /// - Parameter NewGameScore: The new game score.
     func SetGameScore(NewGameScore: Int)
     
     /// Shows and stars the visual indicator to the next game.
-    ///
     /// - Parameter Seconds: Number of seconds between game over and the start of the new game.
     func StartNewGameIn(Seconds: Double)
     
