14e55d6a757715d73f883b2668de151ecf2afee2
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:36:10 +0900

    Update ThreeDProtocol.swift
    Added comments.

--- Common/GameViews/3DView/ThreeDProtocol.swift
+++ Common/GameViews/3DView/ThreeDProtocol.swift
@@ -9,11 +9,28 @@
 import Foundation
 import SceneKit
 
+/// Protocol for debugging View3D objects.
 protocol ThreeDProtocol: class
 {
+    /// Set camera data.
+    /// - Parameter FOV: New field of view.
+    /// - Parameter Position: New camera position.
+    /// - Parameter Orientation: New camera orientation.
     func SetCameraData(FOV: CGFloat, Position: SCNVector3, Orientation: SCNVector4)
+    
+    /// Return camera data.
+    /// - Returns: Tuple with the field of view, position, and orientation.
     func GetCameraData() -> (CGFloat, SCNVector3, SCNVector4)
+    
+    /// Set the main game light.
+    /// - Parameter Position: Position of the light.
+    /// - Parameter LightingType: Type of light.
+    /// - Parameter ColorName: Name of the light color.
+    /// - Parameter UseDefault: Use default light.
     func SetLightData(Position: SCNVector3, LightingType: SCNLight.LightType, ColorName: String,
                       UseDefault: Bool)
+    
+    /// Returns game light information.
+    /// - Returns: Tuple with the position, light type, color name, and use default camera flag.
     func GetLightData() -> (SCNVector3, SCNLight.LightType, String, Bool)
 }
