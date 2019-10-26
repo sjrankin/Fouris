538807ff8aa4e4e42b742b53ed29204de67ff474
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 20:25:47 +0900

    Update DebugDelegate.swift
    Updated comments.

--- Common/Protocols/DebugDelegate.swift
+++ Common/Protocols/DebugDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  DebugDelegate.swift
-//  WackyDesktopTetris
+//  Fouris
 //
 //  Created by Stuart Rankin on 4/29/19.
 //  Copyright © 2019 Stuart Rankin. All rights reserved.
@@ -13,43 +13,35 @@ import UIKit
 protocol DebugDelegate: class
 {
     /// Set the current AI scoring method.
-    ///
     /// - Parameter Method: The scoring method.
     func SetAIScoring(Method: AIScoringMethods)
     
     /// Sets the default AI scoring method.
-    ///
     /// - Parameter IsDefault: If true, use the default scoring method.
     func SetAIDefaultScoring(IsDefault: Bool)
     
     /// Sets the current metapiece group.
-    ///
     /// - Parameter PieceGroups: Current metapiece group.
     func SetPieceGroups(PieceGroups: MetaPieces)
     
     /// Sets how to select random pieces from metapiece groups.
-    ///
     /// - Parameter Method: Determines how to select random pieces.
     func SetPieceSelection(Method: PieceSelectionMethods)
     
     /// Get the current AI scoring method.
-    ///
     /// - Returns: AI scoring method.
     func GetAIScoring() -> AIScoringMethods
     
     /// Get the default AI scoring method flag.
-    ///
     /// - Returns: True if should use the default AI scoring method, false if not.
     func GetAIDefaultScoring() -> Bool
     
     /// Get the current metapiece group.
-    ///
     /// - Returns: current metapiece group.
     func GetPieceGroups() -> MetaPieces
 }
 
 /// How to group pieces for random selection.
-///
 /// - Cumulative: All pieces from Standard to the selected metapiece type are included.
 /// - Exclusive: Only the selected metapiece type is used.
 /// - RandomInSequence: Select a random piece from each metapiece group, in sequence.
