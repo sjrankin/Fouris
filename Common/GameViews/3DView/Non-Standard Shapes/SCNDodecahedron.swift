aaf8e31e44a0505875727866ce6aa2123524dd94
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:35:48 +0900

    Update SCNDodecahedron.swift
    Added comments and access control.

--- Common/GameViews/3DView/Non-Standard Shapes/SCNDodecahedron.swift
+++ Common/GameViews/3DView/Non-Standard Shapes/SCNDodecahedron.swift
@@ -10,9 +10,11 @@ import Foundation
 import SceneKit
 import UIKit
 
-//https://medium.com/@zxlee618/custom-geometry-in-scenekit-f91464297fd1
+/// Implements a dodecahedron-shaped SCNNode.
+/// - Notes: See [Custom Geometry in SceneKit](https://medium.com/@zxlee618/custom-geometry-in-scenekit-f91464297fd1)
 class SCNDodecahedron: SCNNode
 {
+    /// Initializer.
     override init()
     {
         super.init()
@@ -20,6 +22,8 @@ class SCNDodecahedron: SCNNode
         CommonInitialization()
     }
     
+    /// Initializer.
+    /// - Parameter Radius: Radius of the shape.
     init(Radius: CGFloat)
     {
         super.init()
@@ -27,22 +31,28 @@ class SCNDodecahedron: SCNNode
         CommonInitialization()
     }
     
+    /// Initializer.
+    /// - Parameter coder: See Apple documentation.
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
         CommonInitialization()
     }
     
-    func CommonInitialization()
+    /// Initialization common to all initializers.
+    private func CommonInitialization()
     {
         self.geometry = SCNDodecahedron.Geometry(Radius: _Radius)
     }
     
+    /// Updates the shape with a new radial value.
+    /// - Parameter Radius: New radius value.
     private func UpdateDimensions(Radius: CGFloat)
     {
         CommonInitialization()
     }
     
+    /// Holds the radius. Updates the shape when changed.
     private var _Radius: CGFloat = 1.0
     {
         didSet
@@ -50,6 +60,7 @@ class SCNDodecahedron: SCNNode
             UpdateDimensions(Radius: _Radius)
         }
     }
+    /// Get or set the radial value of the shape. Defaults to 1.0.
     public var Radius: CGFloat
     {
         get
@@ -62,8 +73,10 @@ class SCNDodecahedron: SCNNode
         }
     }
     
+    /// Holds the vertices of the shape.
     private static var Vertices = [SCNVector3]()
     
+    /// Holds the original vertices of the shape (with a radius of 1.0).
     private static let OriginalVertices: [SCNVector3] =
         [
             SCNVector3(0.0, 1.0, 0.0),
@@ -74,6 +87,7 @@ class SCNDodecahedron: SCNNode
             SCNVector3(0.0, -1.0, 0.0)
     ]
     
+    /// Holds the indices of the vertices that defines the shape.
     private static let Indices: [UInt16] =
         [
             0, 1, 2,
@@ -86,18 +100,25 @@ class SCNDodecahedron: SCNNode
             4, 5, 1
     ]
     
+    /// Holds the source geometry.
     private static var GeoSource: SCNGeometrySource!
+    /// Holds the geometric element.
     private static var GeoElement: SCNGeometryElement!
     
+    /// Returns geometry that defines a dodecahedron.
+    /// - Parameter Radius: The size of the dodecahedron.
+    /// - Returns: SCNGeometry object with a dodecahedron.
     public static func Geometry(Radius: CGFloat) -> SCNGeometry
     {
         Vertices.removeAll()
+        #if false
         let OperationalRadius = Radius
         for Vertex in OriginalVertices
         {
             //let NewVertex = SCNVector3(Vertex.x * OperationalBase, Vertex.y * OperationalHeight, Vertex.z * OperationalBase)
             //Vertices.append(NewVertex)
         }
+        #endif
         GeoSource = SCNGeometrySource(vertices: Vertices)
         GeoElement = SCNGeometryElement(indices: Indices, primitiveType: .triangles)
         let TextureCoordinates =
