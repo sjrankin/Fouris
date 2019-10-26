a3aaacb3818370b772d9ec9452d87026a3e6759e
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:51:39 +0900

    Update XMLDeserializeProtocol.swift
    Added comments.

--- Common/DataManagers/XMLDocument/XMLDeserializeProtocol.swift
+++ Common/DataManagers/XMLDocument/XMLDeserializeProtocol.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 
+/// Deserialization protocol for XML documents.
 protocol XMLDeserializeProtocol
 {
+    /// Deserialize a node found in an XML document.
+    /// - Parameter Node: The node to deserialize.
     func DeserializedNode(_ Node: XMLNode)
 }
