9e13403b7ac0a4f7b90c1f5557a3d4ff3cc13f3f
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:45:56 +0900

    Update GameViewContainer.swift
    Added comments.

--- GameUI/GameViewContainer.swift
+++ GameUI/GameViewContainer.swift
@@ -9,18 +9,24 @@
 import Foundation
 import UIKit
 
+/// Contains the game view. Used to implement the `ParentSizeChangedProtocol`.
 class GameViewContainer: UIView
 {
+    /// Initializer.
+    /// - Parameter frame: The view's frame.
     override init(frame: CGRect)
     {
         super.init(frame: frame)
     }
     
+    /// Initializer.
+    /// - Parameter coder: See Apple documentation.
     required init?(coder: NSCoder)
     {
         super.init(coder: coder)
     }
     
+    /// Set the bounds of the parent. Notifies all sub-views.
     override var bounds: CGRect
         {
         didSet
