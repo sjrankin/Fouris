58b1fd1ca4a02a2861aa115c4158fe524d0ba375
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:44:53 +0900

    Update ParentSizeChangedProtocol.swift
    Added comments.

--- GameUI/ParentSizeChangedProtocol.swift
+++ GameUI/ParentSizeChangedProtocol.swift
@@ -9,7 +9,11 @@
 import Foundation
 import UIKit
 
+/// Protocol for the notification of changed parent sizes.
 protocol ParentSizeChangedProtocol
 {
+    /// Called when a parent's size changes.
+    /// - Parameter Bounds: New bounds rectangle.
+    /// - Parameter Frame: New frame rectangle.
     func NewParentSize(Bounds: CGRect, Frame: CGRect)
 }
