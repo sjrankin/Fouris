e4c209f7697db76e31838e2606607cff6f8eff2b
Stuart Rankin <sjrankin@gmail.com>
2019-10-26 18:49:46 +0900

    Update TitleTableCell.swift
    Added comments and access control.

--- GameUI/Dialogs/About/TitleTableCell.swift
+++ GameUI/Dialogs/About/TitleTableCell.swift
@@ -9,15 +9,22 @@
 import Foundation
 import UIKit
 
+/// Table view cell for attribute data titles.
 class TitleTableCell: UITableViewCell
 {
+    /// The height of each table cell.
     public static let CellHeight: CGFloat = 50.0
     
+    /// Initializer.
+    /// - Parameter coder: See Apple documentation.
     required init?(coder: NSCoder)
     {
         super.init(coder: coder)
     }
     
+    /// initializer.
+    /// - Parameter style: Style of the table view cell.
+    /// - Parameter reuseIdentifier: Identifier string for the reuse mechanism.
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?)
     {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -26,14 +33,19 @@ class TitleTableCell: UITableViewCell
         TitleLabel?.font = UIFont(name: "Futura", size: 18.0)
     }
     
-    var TitleLabel: UILabel? = nil
+    /// Title label.
+    private var TitleLabel: UILabel? = nil
 
-    func Initialize(WithTitle: String, TableWidth: CGFloat)
+    /// Initialize the table view cell.
+    /// - Parameter WithTitle: Title text.
+    /// - Parameter TableWidth: Width of the table to ensure text does not look too ugly.
+    public func Initialize(WithTitle: String, TableWidth: CGFloat)
     {
         TitleLabel?.frame = CGRect(x: 10, y: 5, width: TableWidth - 20, height: 40)
         TitleLabel?.text = WithTitle
         CellTitle = WithTitle
     }
     
-    var CellTitle: String = ""
+    /// Holds the cell title.
+    public var CellTitle: String = ""
 }
