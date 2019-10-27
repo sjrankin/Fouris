61757a9e92dd584a3e47a2d0d63ef4e6a9e6d47d
Stuart Rankin <sjrankin@gmail.com>
2019-10-27 22:17:46 +0900

    Update GradientObserver.swift
    Added comments.

--- GameUI/Dialogs/GradientPicker/GradientObserver.swift
+++ GameUI/Dialogs/GradientPicker/GradientObserver.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
+/// Protocol to observe changes in gradients.
 protocol GradientObserver: class
 {
+    /// Called when a gradient is changed.
+    /// - Parameter NewGradient: The new gradient definition.
     func GradientChanged(NewGradient: String)
 }
