18cf0f1f2e451e17466df39baaccac2b8177bb4e
Stuart Rankin <sjrankin@gmail.com>
2019-10-27 12:50:53 +0900

    Update SelectBackgroundImageCode.swift
    Added comments and access control.

--- GameUI/Dialogs/Background/SelectBackgroundImageCode.swift
+++ GameUI/Dialogs/Background/SelectBackgroundImageCode.swift
@@ -9,13 +9,17 @@
 import Foundation
 import UIKit
 
+/// Code to run the select background image UI.
 class SelectBackgroundImageCode: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
     ThemeEditingProtocol
 {
-    weak var ThemeDelegate: ThemeEditingProtocol? = nil
-    var ImagePicker: UIImagePickerController? = nil
+    /// Delegate to receive messages from this class.
+    public weak var ThemeDelegate: ThemeEditingProtocol? = nil
+    /// Holds the system image picker controller.
+    public var ImagePicker: UIImagePickerController? = nil
     
-    override func viewDidLoad()
+    /// Initialize the UI.
+    override public func viewDidLoad()
     {
         super.viewDidLoad()
         if let UserBackgroundImage = FileIO.GetSampleImage()
@@ -28,43 +32,58 @@ class SelectBackgroundImageCode: UIViewController, UIImagePickerControllerDelega
         }
     }
     
-    func EditTheme(Theme: ThemeDescriptor2)
+    /// Theme to edit from the caller.
+    /// - Parameter Theme: The theme to edit.
+    public func EditTheme(Theme: ThemeDescriptor2)
     {
         UserTheme = Theme
     }
     
-    func EditTheme(Theme: ThemeDescriptor2, PieceID: UUID)
+    /// Theme to edit from the caller.
+    /// - Parameter Theme: The theme to edit.
+    /// - Parameter PieceID: Not used.
+    public func EditTheme(Theme: ThemeDescriptor2, PieceID: UUID)
     {
         UserTheme = Theme
     }
     
-    var UserTheme: ThemeDescriptor2!
+    /// Theme being edited.
+    private var UserTheme: ThemeDescriptor2!
     
-    func EditResults(_ Edited: Bool, ThemeID: UUID, PieceID: UUID?)
+    /// Not used in this class.
+    public func EditResults(_ Edited: Bool, ThemeID: UUID, PieceID: UUID?)
     {
         //Not used in this class.
     }
     
     @IBOutlet weak var ImageDisplay: UIImageView!
     
-    @IBAction func HandleOKPressed(_ sender: Any)
+    /// Handle the OK button pressed. Notify the caller of a new image. Close the dialog.
+    /// - Parameter sender: Not used.
+    @IBAction public func HandleOKPressed(_ sender: Any)
     {
         ThemeDelegate?.EditResults(true, ThemeID: UserTheme.ID, PieceID: nil)
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func HandleCancelPressed(_ sender: Any)
+    /// Handle the cancel button pressed. Notify the caller of the cancellation. Close the dialog.
+    /// - Parameter sender: Not used.
+    @IBAction public func HandleCancelPressed(_ sender: Any)
     {
         ThemeDelegate?.EditResults(false, ThemeID: UserTheme.ID, PieceID: nil)
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func HandleSetDefaultImage(_ sender: Any)
+    /// Handle the set default image command. Shows the abstract default image.
+    /// - Parameter sender: Not used.
+    @IBAction public func HandleSetDefaultImage(_ sender: Any)
     {
         ImageDisplay.image = UIImage(named: "DefaultImage")
     }
     
-    @IBAction func HandleSelectFromPhotoRoll(_ sender: Any)
+    /// Runs the system image picker.
+    /// - Parameter sender: Not used.
+    @IBAction public func HandleSelectFromPhotoRoll(_ sender: Any)
     {
         ImagePicker = UIImagePickerController()
         ImagePicker?.delegate = self
@@ -75,12 +94,11 @@ class SelectBackgroundImageCode: UIViewController, UIImagePickerControllerDelega
     
     /// Completion block for saving images to the photo roll. Will display an error message if the save was unsuccessful, and
     /// a "saved OK" message if there was no error.
-    ///
     /// - Parameters:
     ///   - image: Not used.
     ///   - error: Error information for when errors occur.
     ///   - contextInfo: Not used.
-    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer)
+    @objc public func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer)
     {
         if let SaveError = error
         {
@@ -99,11 +117,10 @@ class SelectBackgroundImageCode: UIViewController, UIImagePickerControllerDelega
     /// Handle the image picker completion. On successful selection of a new image, the image will be saved to a special
     /// directory where it can be retrieved at will. The image will also be immediately used for the sample image. On error,
     /// an alert is shown to let the user know there was an issue.
-    ///
     /// - Parameters:
     ///   - picker: The UIImagePickerController. Will be dismissed at end of function.
     ///   - info: Dictionary that contains the image (or not, if the user canceled).
-    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    @objc public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
     {
         if let PickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         {
