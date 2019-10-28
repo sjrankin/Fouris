1854cc553a0f8b68c2fdfe695d752b7f3bb29eeb
Stuart Rankin <sjrankin@gmail.com>
2019-10-28 13:49:04 +0900

    Update GradientStopCell.swift
    Added comments and access control.

--- GameUI/Dialogs/GradientPicker/GradientStopCell.swift
+++ GameUI/Dialogs/GradientPicker/GradientStopCell.swift
@@ -10,18 +10,27 @@
 import Foundation
 import UIKit
 
+/// UI for a table view cell for a color stop for a gradient.
 class GradientStopCell: UITableViewCell
 {
+    /// Cell height.
     public static var CellHeight: CGFloat = 50.0
     
+    /// Required initializer.
+    /// - Parameter coder: See Apple documentation.
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
     }
     
-    var ColorSample: UIView!
-    var LocationLabel: UILabel!
+    /// Color sample view.
+    private var ColorSample: UIView!
+    /// Location label.
+    private var LocationLabel: UILabel!
     
+    /// Initializer. Sets up the UI.
+    /// - Parameter style: Style of the table view cell.
+    /// - Parameter reuseIdentifier: Identifier for table view cell reuse and caching.
     override init(style Style: UITableViewCell.CellStyle, reuseIdentifier ReuseIdentifier: String?)
     {
         super.init(style: Style, reuseIdentifier: ReuseIdentifier)
@@ -45,7 +54,10 @@ class GradientStopCell: UITableViewCell
         self.selectionStyle = .none
     }
     
-    func SetData(StopColor: UIColor, StopLocation: Double)
+    /// Initializes the table view cell with a color stop.
+    /// - Parameter StopColor: The color for the color stop.
+    /// - Parameter StopLocation: The (normalized) location of the color stop.
+    public func SetData(StopColor: UIColor, StopLocation: Double)
     {
         ColorSample.backgroundColor = StopColor
         LocationLabel.text = "\(StopLocation.Round(To: 3))"
@@ -53,9 +65,13 @@ class GradientStopCell: UITableViewCell
         _CellLocation = StopLocation
     }
     
-    var _CellColor = UIColor.black
-    var _CellLocation: Double = 0.0
+    /// The color of the color stop.
+    private var _CellColor = UIColor.black
+    /// The location of the color stop.
+    private var _CellLocation: Double = 0.0
     
+    /// Returns the data in the cell.
+    /// - Returns: Tuple of the color and the location.
     func CellData() -> (UIColor, Double)
     {
         return (_CellColor, _CellLocation)
