e007f3617ebb967a4b46dc7a82adf1b69e9682f9
Stuart Rankin <sjrankin@gmail.com>
2019-11-04 14:27:59 +0900

    Update +Rotations.swift
    Moved functions around. Added Duration to StopShowingOff.

--- Common/GameViews/3DView/3DViewExtensions/+Rotations.swift
+++ Common/GameViews/3DView/3DViewExtensions/+Rotations.swift
@@ -13,6 +13,8 @@ import SceneKit
 /// Functions related to board rotations. Also contains showing-off functions.
 extension View3D
 {
+    // MARK: - Board rotation functions.
+    
     /// Rotates the contents of the game (but not UI or falling piece) on the specified axis. The contents are rotated to an
     /// absolute angle.
     /// - Note:
@@ -96,78 +98,6 @@ extension View3D
         BucketNode?.runAction(RotateBy)
     }
     
-    /// Stop showing off rotations.
-    public func StopShowingOff()
-    {
-        print("StopShowingOff")
-        let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
-        if !BoardDef!.BucketRotates
-        {
-            // Do not waste time if the bucket doesn't rotate.
-            print("StopShowingOff canceled - bucket doesn't rotate.")
-            return
-        }
-        
-        //Destroy the show off timer.
-        ShowOffTimer?.invalidate()
-        ShowOffTimer = nil
-        
-        //Remove node actions.
-        if CurrentTheme!.RotateBucketGrid
-        {
-            BucketGridNode?.removeAllActions()
-            OutlineNode?.removeAllActions()
-        }
-        MasterBlockNode?.removeAllActions()
-        BucketNode?.removeAllActions()
-        
-        //Move to an ordinal position.
-        let Reset = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: 1.0)
-        if CurrentTheme!.RotateBucketGrid
-        {
-            BucketGridNode?.runAction(Reset)
-            OutlineNode?.runAction(Reset)
-        }
-        MasterBlockNode?.runAction(Reset)
-        BucketNode?.runAction(Reset)
-    }
-    
-    /// Show off rotations. Intended for use by the game after game over to provide visual interest.
-    /// - Note: Rotations are selected randomly
-    /// - Parameter Duration: Time for one rotation.
-    /// - Parameter Delay: Time between rotations.
-    public func ShowOffRotations(Duration: Double, Delay: Double)
-    {
-        let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
-        if !BoardDef!.BucketRotates
-        {
-            // Do not waste time if the bucket doesn't rotate.
-            return
-        }
-        //Reset rotatable objects to a known rotation to keep things in sync with each other.
-        let Reset = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: 0.01)
-        MasterBlockNode?.runAction(Reset)
-        BucketNode?.runAction(Reset)
-        if CurrentTheme!.RotateBucketGrid
-        {
-            BucketGridNode?.runAction(Reset)
-            OutlineNode?.runAction(Reset)
-        }
-        //Run the first execution prior to setting up the timer so that things do not appear to stall.
-        ExecutionRotation()
-        ShowOffTimer = Timer.scheduledTimer(timeInterval: Delay, target: self, selector: #selector(ExecutionRotation),
-                                            userInfo: nil, repeats: true)
-    }
-    
-    /// Execution a rotation of the board game to show off after game over.
-    @objc func ExecutionRotation()
-    {
-        let Axis = Axes.allCases.randomElement()!
-        let Angle: CGFloat = (Axis == .ZAxis ? 90.0 : 180.0) * CGFloat([1.0, -1.0].randomElement()!)
-        let RotationalDuration: Double = Axis == .ZAxis ? 0.3 : 0.6
-        RotateContentsBy(OnAxis: Axis, By: Angle, Duration: RotationalDuration)
-    }
-    
     /// Rotates the contents of the game (but not UI or falling piece) by the specified number of degrees.
     /// - Note:
     ///   - This function uses a synchronous lock to make sure that when the board is rotating, other things don't happen to it.
@@ -181,7 +111,7 @@ extension View3D
         objc_sync_enter(RotateLock)
         defer{objc_sync_exit(RotateLock)}
         
-        let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
+        //let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
         
         let DirectionalSign = CGFloat(Right ? -1.0 : 1.0)
         RotationCardinalIndex = RotationCardinalIndex + 1
@@ -227,5 +157,80 @@ extension View3D
     public func RotateContentsLeft(Duration: Double = 0.33, Completed: @escaping (() -> Void))
     {
         RotateContents(Right: false, Duration: Duration, Completed: Completed)
+    }
+    
+    // MARK: - Showing-off functions.
+    
+    /// Stop showing off rotations.
+    /// - Parameter Duration: Number of seconds to take to rotate the board back to a neutral position.
+    public func StopShowingOff(Duration: Double = 1.0)
+    {
+        print("StopShowingOff")
+        let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
+        if !BoardDef!.BucketRotates
+        {
+            // Do not waste time if the bucket doesn't rotate.
+            print("StopShowingOff canceled - bucket doesn't rotate.")
+            return
+        }
+        
+        //Destroy the show off timer.
+        ShowOffTimer?.invalidate()
+        ShowOffTimer = nil
+        
+        //Remove node actions.
+        if CurrentTheme!.RotateBucketGrid
+        {
+            BucketGridNode?.removeAllActions()
+            OutlineNode?.removeAllActions()
+        }
+        MasterBlockNode?.removeAllActions()
+        BucketNode?.removeAllActions()
+        
+        //Move to an ordinal position.
+        let Reset = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: Duration)
+        if CurrentTheme!.RotateBucketGrid
+        {
+            BucketGridNode?.runAction(Reset)
+            OutlineNode?.runAction(Reset)
+        }
+        MasterBlockNode?.runAction(Reset)
+        BucketNode?.runAction(Reset)
+    }
+    
+    /// Show off rotations. Intended for use by the game after game over to provide visual interest.
+    /// - Note: Rotations are selected randomly
+    /// - Parameter Duration: Time for one rotation.
+    /// - Parameter Delay: Time between rotations.
+    public func ShowOffRotations(Duration: Double, Delay: Double)
+    {
+        let BoardDef = BoardManager.GetBoardFor(CenterBlockShape!)
+        if !BoardDef!.BucketRotates
+        {
+            // Do not waste time if the bucket doesn't rotate.
+            return
+        }
+        //Reset rotatable objects to a known rotation to keep things in sync with each other.
+        let Reset = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: 0.01)
+        MasterBlockNode?.runAction(Reset)
+        BucketNode?.runAction(Reset)
+        if CurrentTheme!.RotateBucketGrid
+        {
+            BucketGridNode?.runAction(Reset)
+            OutlineNode?.runAction(Reset)
+        }
+        //Run the first execution prior to setting up the timer so that things do not appear to stall.
+        ExecutionRotation()
+        ShowOffTimer = Timer.scheduledTimer(timeInterval: Delay, target: self, selector: #selector(ExecutionRotation),
+                                            userInfo: nil, repeats: true)
+    }
+    
+    /// Execution a rotation of the board game to show off after game over.
+    @objc func ExecutionRotation()
+    {
+        let Axis = Axes.allCases.randomElement()!
+        let Angle: CGFloat = (Axis == .ZAxis ? 90.0 : 180.0) * CGFloat([1.0, -1.0].randomElement()!)
+        let RotationalDuration: Double = Axis == .ZAxis ? 0.3 : 0.6
+        RotateContentsBy(OnAxis: Axis, By: Angle, Duration: RotationalDuration)
     }
 }
